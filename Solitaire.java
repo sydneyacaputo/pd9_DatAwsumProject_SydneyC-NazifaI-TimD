73bfcb9eb34bc67ae03d3f77bb6fc3b64529b589
nislam17 <nislam17@gmail.com>
2014-05-27 21:31:15 -0400

    Added the main and potential methods for this class

---
+++ Solitaire.java
@@ -0,0 +1,7 @@
+public class Solitaire {
+	//method for dealing 
+	//uhhhhh
+	public static void main(String[] args) {
+		System.out.println("HELLO, THE GAME IS BEGINNING.")
+	}//end main 
+}//end class Solitaire 
