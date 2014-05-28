98b7b92b9d32e242556c004f0f32afe2a7a1bfde
nislam17 <nislam17@gmail.com>
2014-05-27 22:24:52 -0400

    edited all methods

--- Ranks.java
+++ Ranks.java
@@ -1,26 +1,55 @@
 
 public class Ranks implements Pile {
 	
-	int _size;
-	Card _head, _end; 
+	private int _size;
+	private Card _head, _tail; 
 	
 	public Ranks {
-		_size = 0
-		_head = null;
+		_size = 0;
+		_head = _tail = null;
 	}
 	
 	public Card peek() {
 		return _end
 	}
 	
-	void add( Card c )
+	void add( Card c ) {
+		_tail = new Card(); 
+		
+		if ( _size == 0 ) 
+			_head = _tail;
+		else 
+			_tail.getPrev().setNext( _tail ); //have these methods in Card? 
+		_size++;
+	}
 	
-	Card remove()
+	public Card remove() {
+		Card ret = _tail; 
+		//if isEmpty? 
+		if ( size() == 1) {
+			_head = _tail = null; 
+		}
+		else {
+			_tail = _tail.getPrev(); 
+			_tail.setNext( null );
+		}
+		size--;
+		return ret; 
+	}
 	
 	int getSize() {
 		return _size; 
 	}
 	
-	public String toString()
+	public String toString() {
+		String retStr  "HEAD->"; 
+		Card tmp = _head; 
+		while ( tmp !=null ) {
+			//retStr = tmp.getWhateverTheValue + "->";
+			tmp = tmp.getNext(); 
+		}
+		retStr += "NULL";
+		return retStr; 
+	}
 	
 }//end class
