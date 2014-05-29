4f071b44f441f7d60c5492a755a9efee89e7b3dd
tdiep26 <timd333@gmail.com>
2014-05-29 14:24:21 -0400

    renamed deck

---
+++ DrawPile.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+public class DrawPile implements Pile {
+
+    private ArrayList<Card> _deck;
+    private int _size;
+
+    public DrawPile() {
+	_deck = new ArrayList<Card>();
+	
+	for ( int i=0; i<52; i++ ) {
+	    if ( i < 13 )
+	        add( new Card(i+1,"Spade") );
+	    else if ( i < 26 )
+	        add( new Card(i-12,"Heart") );
+	    else if ( i < 39 )
+	        add( new Card(i-25,"Club") );
+	    else 
+	        add( new Card(i-38,"Diamond") );
+	}
+	//how are we accounting for the different face cards?
+	//also should i exclude the color because that is known by the suit?
+	for ( int i=52; i>0; i-- ) {
+	    int j = (int) Math.random() * i;
+	    Card temp = _deck.get(i);
+	    _deck.set( i, _deck.get(j) );
+	    _deck.set( j, temp );
+	}	    
+    }
+
+    public Card peek() {
+	return _deck.get( _size - 1 );
+    }
+
+    public void add( Card c ) {
+	_deck.add(c);
+	_size++;
+    }
+
+    public Card remove() {
+	_size--;
+	return _deck.remove(0);
+    }
+
+    public int getSize() {
+	return _size;
+    }
+
+    /*
+    public String toString() {
+	String ret = "";
+	for ( Card c : _deck )
+	    ret = ret + c + " ";
+	ret = ret.substring( 0, _deck.size()*2 - 1 );
+	return ret;
+    }
+    */
+
+    public void deal( Ranks r1, Ranks r2, Ranks r3, Ranks r4, Ranks r5,Ranks r6,Ranks r7 ) {
+        Ranks[] field = { r1, r2, r3, r4, r5, r6, r7 };
+	for ( int i=0; i<7; i++ ) {
+	    for ( int j=i; j<7; j++ )
+		field[j] = remove();
+	}
+x    }
+
+    public static void main( String[] args ) {
+
+	DrawPile test = new DrawPile();
+	System.out.println(test);
+	
+	System.out.println(test.peek());
+	System.out.println(test.remove());
+	System.out.println(test);
+
+    }
+	
+}
